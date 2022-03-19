e18f6b721885c5e60e7a4eb93bf2698994d0d694
Lauri Klemettilä <44393748+LauriKle@users.noreply.github.com>
2022-03-19 13:57:46 +0200

    Improved Conveyor Belt

--- Project/CVD_Sim_And_Empathy/Assets/Scripts/ConveyorScript.cs
+++ Project/CVD_Sim_And_Empathy/Assets/Scripts/ConveyorScript.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class ConveyorScript : MonoBehaviour
 {
+    public Vector2 scroll = new Vector2 (0.05f , 0.05f);
+    Vector2 offset = new Vector2 (0f, 0f);
     
     public float conveyorSpeed = 500.0f;
     public Vector3 conveyorDirection;
     
-    private Vector3 direction;
+    public List<GameObject> itemsToConvey;
     
-    void OnCollisionStay(Collision other) {
+    
+   // private Vector3 direction;
+    
+    void Update () {
+        //Scroll texture
+        offset +=  scroll * Time.deltaTime;
+        GetComponent<Renderer>().material.SetTextureOffset ("_MainTex", offset);
+        
+        //Move items on the conveyor
+        int l = itemsToConvey.Count;
+        if (l > 0){
+            foreach (GameObject item in itemsToConvey){
+                Rigidbody rigidbody = item.GetComponent<Rigidbody>();
+                rigidbody.velocity = conveyorSpeed * conveyorDirection * Time.fixedDeltaTime ;
+            }
+        }
+    }
+
+
+    void OnCollisionEnter(Collision other) {
         if (other.gameObject.layer == 6){
-            Rigidbody rigidbody = other.gameObject.GetComponent<Rigidbody>();
-            rigidbody.velocity = conveyorSpeed * conveyorDirection * Time.deltaTime ;
+            itemsToConvey.Add(other.transform.gameObject);
+        }
+    }
+    
+    void OnCollisionExit(Collision other) {
+        if (itemsToConvey.Contains(other.transform.gameObject)){
+            itemsToConvey.Remove(other.transform.gameObject);
         }
     }
 }
